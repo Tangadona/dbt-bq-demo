535e4005748f5f81f63a298b82bb9b9d6504c4dc
t.shaikshavali@dizitiveit.com <t.shaikshavali@dizitiveit.com>
2025-03-09 00:52:04 +0000

    deleted old files

--- models/dbttraining.sql
+++
@@ -1,9 +0,0 @@
-With stg_customers AS(
-    select 
-        cusomter Id,
-        concat(First Name,' ',Last Name) as customer_name,
-        Email as email_address
-    from sql_tutorial.Customers
-)
-
-select * from stg_customers
--- models/org1.sql
+++
@@ -1,9 +0,0 @@
-with stg_training as(
-    select
-        Index,
-        Name,
-        Country 
-    from sql_tutorial.organizations
-)
-
-select * from stg_training
